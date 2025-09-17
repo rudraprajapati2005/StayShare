1662a996de96bccfe3887e536400640a637697e7
Rudra <rudraprajapati5102005@gmail.com>
2025-09-17 12:42:30 +0530

    Repository added

---
+++ Models/IRoomRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using StayShare.Models;
+
+namespace StayShare.Repositories
+{
+    public interface IRoomRepository
+    {
+        Task<Room> GetRoomByIdAsync(int roomId);
+        Task<IEnumerable<Room>> GetAvailableRoomsAsync();
+        Task<IEnumerable<RoomOccupancy>> GetCurrentOccupantsAsync(int roomId);
+        Task AddRoomAsync(Room room);
+        Task SaveChangesAsync();
+    }
+}
